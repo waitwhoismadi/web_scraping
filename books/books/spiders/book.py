d5187ddf7a76a5815ac44612d252e674bc7f2157
waitwhoismadi <55494675+waitwhoismadi@users.noreply.github.com>
2025-10-21 19:05:56 +0500

    contracts added

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -6,7 +6,19 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
+    def start_requests(self):
+        for url in self.start.urls:
+            yield scrapy.Request(
+                url, callback=self.parse, errback=self.log_error
+            )
+
     def parse(self, response):
+        """
+        @url https://books.toscrape.com
+        @returns items 20 20
+        @returns request 1 50
+        @scrapes url title price
+        """
         for book in response.css("article.product_pod"):
             item = BooksItem()
             item["url"] = book.css("h3 > a::attr(href)").get()
@@ -20,4 +32,11 @@ class BookSpider(scrapy.Spider):
             self.logger.info(
                 f"Navigating to next page with URL {next_page_url}."
             )
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
+            yield scrapy.Request(
+                url=next_page_url, 
+                callback=self.parse,
+                errback=self.log_error
+                )
+
+    def log_error(self, failure):
+        self.logger.error(repr(failure))
